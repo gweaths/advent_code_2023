d09690b66fbc40d1cc711688b7ae791072365eda
gweaths <grant.w2011@gmail.com>
2023-12-03 01:16:28 +0000

    Day 2 - Part 1 complete

---
+++ Day_2/GameSets.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Day_2;
+
+public class GameSets
+{
+  public required int GameNumber { get; set; }
+  public required string[] Sets { get; set; }
+}
+
+
--- Day_2/Program.cs
+++ Day_2/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text;
+using Day_2;
+
+
+
+Dictionary<string, int> maxValues = new()
+{
+    { "red", 12 },
+    { "green", 13 },
+    { "blue", 14 }
+};
+
+bool isValidGame(Game game)
+{
+  return game.sets.All(isValidSet);
+}
+
+bool isValidSet(GameSet set)
+{
+  return set.red <= maxValues["red"] && set.green <= maxValues["green"] && set.blue <= maxValues["blue"];
+
+}
+
+int Challenge_Part1(Game[] games)
+{
+  return games.Where(isValidGame).Select(x => x.gameId).Sum();
+}
+
+string[] lines = File.ReadAllLines("./test.txt");
+Game[] games = lines.Select(Game.Parse).ToArray();
+Console.WriteLine(Challenge_Part1(games));
+
+
+
+
+
+
+
+
+
+
+
