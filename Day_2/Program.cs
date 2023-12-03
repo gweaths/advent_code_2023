535fac3b448e1cc8faffc1325f544e6da9cb4b87
gweaths <grant.w2011@gmail.com>
2023-12-03 01:26:10 +0000

    Day 2 - Part 1 complete

--- Day_2/Program.cs
+++ Day_2/Program.cs
@@ -21,14 +21,37 @@ bool isValidSet(GameSet set)
 
 }
 
+GameSet FindMaxSets(List<GameSet> sets)
+{
+  int red = 0;
+  int green = 0;
+  int blue = 0;
+  foreach (var set in sets)
+  {
+    red = Math.Max(set.red, red);
+    green = Math.Max(set.green, green);
+    blue = Math.Max(set.blue, blue);
+  }
+
+  return new GameSet(red: red, green: green, blue: blue);
+}
+
 int Challenge_Part1(Game[] games)
 {
   return games.Where(isValidGame).Select(x => x.gameId).Sum();
 }
 
+int Challenge_Part2(Game[] games)
+{
+  return games.Select(game => FindMaxSets(game.sets)).Select(x => x.red * x.green * x.blue).Sum();
+}
+
 string[] lines = File.ReadAllLines("./test.txt");
 Game[] games = lines.Select(Game.Parse).ToArray();
+
+
 Console.WriteLine(Challenge_Part1(games));
+Console.WriteLine(Challenge_Part2(games));
 
 
 
