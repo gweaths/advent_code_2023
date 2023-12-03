803de42bad17d494503d3f7b790ba3732501d118
Grant W <12433404+gweaths@users.noreply.github.com>
2023-12-03 01:31:16 +0000

    Day2 - Working & Complete

--- Day_2/Program.cs
+++ Day_2/Program.cs
@@ -1,2 +1,62 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Day_2;
+
+Dictionary<string, int> maxValues = new()
+{
+    { "red", 12 },
+    { "green", 13 },
+    { "blue", 14 }
+};
+
+bool isValidGame(Game game)
+{
+  return game.sets.All(isValidSet);
+}
+
+bool isValidSet(GameSet set)
+{
+  return set.red <= maxValues["red"] && set.green <= maxValues["green"] && set.blue <= maxValues["blue"];
+
+}
+
+GameSet FindMaxSets(List<GameSet> sets)
+{
+  int red = 0;
+  int green = 0;
+  int blue = 0;
+  foreach (var set in sets)
+  {
+    red = Math.Max(set.red, red);
+    green = Math.Max(set.green, green);
+    blue = Math.Max(set.blue, blue);
+  }
+
+  return new GameSet(red: red, green: green, blue: blue);
+}
+
+int Challenge_Part1(Game[] games)
+{
+  return games.Where(isValidGame).Select(x => x.gameId).Sum();
+}
+
+int Challenge_Part2(Game[] games)
+{
+  return games.Select(game => FindMaxSets(game.sets)).Select(x => x.red * x.green * x.blue).Sum();
+}
+
+string[] lines = File.ReadAllLines("./test.txt");
+Game[] games = lines.Select(Game.Parse).ToArray();
+
+
+Console.WriteLine(Challenge_Part1(games));
+Console.WriteLine(Challenge_Part2(games));
+
+
+
+
+
+
+
+
+
+
+
