e4b7eab0b5ee5d6e2ff17ac8981a49bba629abd7
gweaths <grant.w2011@gmail.com>
2023-12-09 23:18:19 +0000

    Day 8 pt2: incomplete brute force timeout)

--- Day8/Program.cs
+++ Day8/Program.cs
@@ -8,10 +8,11 @@ class Program
 {
   static void Main()
   {
-    SolvePuzzle("puzzle.txt");
+    // Part1("puzzle.txt");
+    Part2("puzzle.txt");
   }
 
-  static void SolvePuzzle(string filePath)
+  static void Part1(string filePath)
   {
     var lines = File.ReadAllLines(filePath);
 
@@ -34,19 +35,49 @@ class Program
     Console.WriteLine(stepsTaken);
   }
 
-  public record NextStep(string Left, string Right)
+  static void Part2(string filePath)
   {
-    private static readonly Regex regex = new Regex(@"(?<key>\w{3}) = \((?<left>\w{3}), (?<right>\w{3})\)");
+    var lines = File.ReadAllLines(filePath);
+    char[] instructions = lines[0].ToCharArray();
+    var nodes = lines.Skip(2).Select(NextStep.Parse).ToList();
+    Dictionary<string, NextStep> mappings = nodes.ToDictionary(node => node.Key, node => node.Step);
 
-    public static (string Key, NextStep Step) Parse(string line)
+    string[] currentPoints = mappings
+          .Where(p => p.Key.EndsWith('A'))
+          .Select(x => x.Key)
+          .ToArray();
+
+    long currentIndex = 0;
+    long stepsTaken = 0;
+    while (!currentPoints.All(x => x.EndsWith('Z')))
     {
-      Match match = regex.Match(line);
+      for (int i = 0; i < currentPoints.Length; i++)
+      {
+        currentPoints[i] = instructions[currentIndex] == 'L' ? mappings[currentPoints[i]].Left : mappings[currentPoints[i]].Right;
 
-      var key = match.Groups["key"].Value;
-      var left = match.Groups["left"].Value;
-      var right = match.Groups["right"].Value;
+      }
+      stepsTaken++;
+      currentIndex = (currentIndex + 1) % instructions.Length;
 
-      return (key, new NextStep(left, right));
     }
+
+    Console.WriteLine(stepsTaken);
   }
 }
+
+public record NextStep(string Left, string Right)
+{
+  private static readonly Regex regex = new Regex(@"(?<key>\w{3}) = \((?<left>\w{3}), (?<right>\w{3})\)");
+
+  public static (string Key, NextStep Step) Parse(string line)
+  {
+    Match match = regex.Match(line);
+
+    var key = match.Groups["key"].Value;
+    var left = match.Groups["left"].Value;
+    var right = match.Groups["right"].Value;
+
+    return (key, new NextStep(left, right));
+  }
+}
+
