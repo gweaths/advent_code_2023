7c723f73818473a47e35425e6e6e4791880073bb
gweaths <grant.w2011@gmail.com>
2024-01-05 18:15:45 +0000

    Day 15 Part 1

---
+++ Day15/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+  static void Main()
+  {
+
+    var input = File.ReadAllText("./input.txt");
+    var splits = input.Split(',', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+    var endResult = splits.Select(ParseString).Sum();
+    Console.WriteLine(endResult.ToString());
+  }
+
+
+  static int ParseString(string str)
+  {
+    var currentValue = 0;
+
+    foreach (var c in str)
+    {
+      currentValue = IncreaseNumber(currentValue, c);
+    }
+
+    return currentValue;
+  }
+
+  static int IncreaseNumber(int currentValue, char character)
+  {
+    var asciiValue = (int)character;
+
+    currentValue += asciiValue;
+    currentValue *= 17;
+    currentValue = currentValue % 256;
+
+    return currentValue;
+  }
+}
